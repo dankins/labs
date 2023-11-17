e7f87789ab6396bc6ccd4f16f89cf30ec7a90bd7
Dan Kinsley <dan@thechorus.xyz>
2023-11-17 15:13:25 -0500

    vercel kv

--- libs/integrations/redis/src/RedisService.ts
+++ libs/integrations/redis/src/RedisService.ts
@@ -1,11 +1,20 @@
 import { RedisClientType, createClient } from "redis";
 import { ZodRawShape, z } from "zod";
+import { kv, createClient as createVercelClient, VercelKV } from "@vercel/kv";
 
-let client: RedisClientType;
-let connect: Promise<RedisClientType>;
+let client: RedisClientType | VercelKV;
+let connect: Promise<unknown>;
 
 export class RedisService {
   constructor(clientOptions: { url: string }) {
+    if (process.env["KV_REST_API_URL"] && process.env["KV_REST_API_TOKEN"]) {
+      client = createVercelClient({
+        url: process.env["KV_REST_API_URL"],
+        token: process.env["KV_REST_API_TOKEN"],
+      });
+      connect = Promise.resolve();
+      return;
+    }
     if (!client) {
       client = createClient(clientOptions);
       connect = client.connect();
@@ -20,7 +29,7 @@ export class RedisService {
     path: string = "$"
   ): Promise<z.infer<typeof schema>> {
     schema.parse(data);
-    const client = await connect;
+    await connect;
     await client.json.set(`${keyspace}:${key}`, path, data);
 
     return data;
@@ -31,7 +40,7 @@ export class RedisService {
     keyspace: string,
     key: string
   ): Promise<z.infer<typeof schema> | null> {
-    const client = await connect;
+    await connect;
     const data = await client.json.get(`${keyspace}:${key}`);
 
     if (!data) {
