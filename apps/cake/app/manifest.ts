9d26d6585a2092a167fb1b6c5c4c04f42b83c108
Dan Kinsley <dan@thechorus.xyz>
2023-11-29 10:00:49 -0500

    manifest

---
+++ apps/cake/app/manifest.ts
@@ -0,0 +1,21 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Cake Prototype",
+    short_name: "Cake Prototype",
+    description:
+      "Proof of concept application to showcase PWA functionality in cake",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+      {
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+    ],
+  };
+}
