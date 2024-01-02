c5e78a4478babe82b5b085a2fcf3e77b94bda9b4
Dan Kinsley <dan@thechorus.xyz>
2024-01-02 15:57:19 -0500

    offer change

--- libs/cake/members/dashboard/src/pass/BrandPass.tsx
+++ libs/cake/members/dashboard/src/pass/BrandPass.tsx
@@ -9,11 +9,26 @@ export async function BrandPass({ brandSlug }: { brandSlug: string }) {
   const result = await getBrandPassOffers(userId, brandSlug);
 
   const pass = result[0].passes;
+  const brand = result[0].brands;
   const offers = result.filter((r) => r.offers !== null).map((r) => r.offers);
   return (
     <div>
-      <div>{JSON.stringify(pass)}</div>
-      <div>{JSON.stringify(offers)}</div>
+      <div>Pass: {brand.slug}</div>
+      <div>
+        {offers.map((o) => (
+          <OfferRow offer={o!} />
+        ))}
+      </div>
     </div>
   );
+}
+
+function OfferRow({
+  offer,
+}: {
+  offer: NonNullable<
+    Awaited<ReturnType<typeof getBrandPassOffers>>[0]["offers"]
+  >;
+}) {
+  return <div>Offer: {offer.id}</div>;
 }
