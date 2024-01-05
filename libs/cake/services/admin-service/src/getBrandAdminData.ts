40bf004a0728e4641daba27c37cec6149e015fee
Dan Kinsley <dan@thechorus.xyz>
2024-01-05 13:13:24 -0500

    recover from not found exception

--- libs/cake/services/admin-service/src/getBrandAdminData.ts
+++ libs/cake/services/admin-service/src/getBrandAdminData.ts
@@ -3,32 +3,35 @@ import { brands, db } from "@danklabs/cake/db";
 
 export async function getBrandAdminData(slug: string) {
   const [dbData, cmsData] = await Promise.all([
-    db.query.brands
-      .findFirst({
-        where: (brand, { eq }) => eq(brand.slug, slug),
-        with: {
-          offerTemplates: true,
-        },
-      })
-      .then(async (b) => {
-        if (!b) {
-          // brand doesn't exist in the database, but maybe it exists in the CMS
-          const cmsBrand = await getBrandAdmin(slug);
-          if (cmsBrand) {
-            await db.insert(brands).values([
-              {
-                slug,
-              },
-            ]);
-          }
-          throw new Error("unable to find brand");
+    getDatabaseData(slug).then(async (b) => {
+      if (!b) {
+        // brand doesn't exist in the database, but maybe it exists in the CMS
+        const cmsBrand = await getBrandAdmin(slug);
+        if (cmsBrand) {
+          await db.insert(brands).values([
+            {
+              slug,
+            },
+          ]);
+          return getDatabaseData(slug).then((b) => b!);
         }
-        return b;
-      }),
+        throw new Error("unable to find brand");
+      }
+      return b;
+    }),
     getBrandAdmin(slug),
   ]);
 
   return { ...dbData, cmsData };
+}
+
+async function getDatabaseData(slug: string) {
+  return db.query.brands.findFirst({
+    where: (brand, { eq }) => eq(brand.slug, slug),
+    with: {
+      offerTemplates: true,
+    },
+  });
 }
 
 export type AdminBrandData = Awaited<ReturnType<typeof getBrandAdminData>>;
