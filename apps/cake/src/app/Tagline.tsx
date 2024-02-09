911533df17e6ef2e835e44dfc2d3a2b94fba75a0
Dan Kinsley <dan@thechorus.xyz>
2024-02-09 17:11:47 -0500

    smaller tagline

--- apps/cake/src/app/Tagline.tsx
+++ apps/cake/src/app/Tagline.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 export function Tagline() {
   return (
     <motion.span
-      className="font-fancy text-[4.5vw] capitalize text-center"
+      className="font-fancy text-[3.5vw] capitalize text-center"
       initial={{ opacity: 0, translateY: 10 }}
       animate={{ opacity: 1, translateY: 0 }}
       transition={{ duration: 1, delay: 2 }}
