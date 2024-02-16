7dbaf65ad4f098b8206edd6fcdfb4bd7e56a06ef
Dan Kinsley <dan@thechorus.xyz>
2024-02-16 13:01:06 -0500

    fix tracking

--- apps/cake/src/app/page.tsx
+++ apps/cake/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
   return (
     <div className="w-full p-4 min-h-screen text-[#FDFDEA] flex flex-col items-center justify-center">
       <div className={classNames("flex flex-col items-center container")}>
-        <h1 className="text-[9.375rem] font-readex uppercase tracking-normal font-bold tracking-[2.34375rem]">
+        <h1 className="text-[9.375rem] font-readex uppercase font-bold tracking-[2.34375rem]">
           Cake
         </h1>
         <h3 className="font-lexend text-[1.25rem] text-center">
