7e65b54d5c74268fce8f56815aa77ab962b750a3
Dan Kinsley <dan@thechorus.xyz>
2024-02-16 13:29:06 -0500

    mobile text size

--- apps/cake/src/app/page.tsx
+++ apps/cake/src/app/page.tsx
@@ -6,10 +6,10 @@ export default function Page() {
   return (
     <div className="w-full p-4 min-h-screen text-[#FDFDEA] flex flex-col items-center justify-center">
       <div className={classNames("flex flex-col items-center container")}>
-        <h1 className="text-[9.375rem] font-readex uppercase font-bold tracking-[2.34375rem]">
+        <h1 className="text-[5rem] tracking-[1.3rem] font-readex uppercase font-bold lg:text-[9.375rem] lg:tracking-[2.34375rem]">
           Cake
         </h1>
-        <h3 className="font-lexend text-[1.25rem] text-center">
+        <h3 className="font-lexend text-sm lg:text-[1.25rem] text-center">
           Shopping is about to get sweeter.
         </h3>
       </div>
