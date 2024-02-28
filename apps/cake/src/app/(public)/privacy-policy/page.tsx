5f2c73acf92bbc1f96dcb3c92deb5260157b8b3e
Dan Kinsley <dan@thechorus.xyz>
2024-02-28 15:00:07 -0500

    fix static pages

--- apps/cake/src/app/(public)/privacy-policy/page.tsx
+++ apps/cake/src/app/(public)/privacy-policy/page.tsx
@@ -1,17 +1,14 @@
 import { getPage } from "@danklabs/cake/cms";
 import { RenderContent } from "@danklabs/cake/cms-content";
 
-// export default async function Page() {
-//   const page = await getPage("privacy-policy");
-
-//   return (
-//     <div className="flex flex-row justify-center w-full">
-//       <div className="prose-dark">
-//         <RenderContent blocks={page.content} />
-//       </div>
-//     </div>
-//   );
-// }
 export default async function Page() {
-  return <div>stub</div>;
+  const page = await getPage("privacy-policy");
+
+  return (
+    <div className="flex flex-row justify-center w-full">
+      <div className="prose-dark">
+        <RenderContent blocks={page.content} />
+      </div>
+    </div>
+  );
 }
--- apps/cake/src/app/(public)/terms-and-conditions/page.tsx
+++ apps/cake/src/app/(public)/terms-and-conditions/page.tsx
@@ -2,17 +2,13 @@ import { getPage } from "@danklabs/cake/cms";
 import { RenderContent } from "@danklabs/cake/cms-content";
 
 export default async function Page() {
-  return <div>stub</div>;
+  const page = await getPage("terms-and-conditions");
+
+  return (
+    <div className="flex flex-row justify-center w-full">
+      <div className="prose-dark">
+        <RenderContent blocks={page.content} />
+      </div>
+    </div>
+  );
 }
-
-// export default async function Page() {
-//   const page = await getPage("terms-and-conditions");
-
-//   return (
-//     <div className="flex flex-row justify-center w-full">
-//       <div className="prose-dark">
-//         <RenderContent blocks={page.content} />
-//       </div>
-//     </div>
-//   );
-// }
