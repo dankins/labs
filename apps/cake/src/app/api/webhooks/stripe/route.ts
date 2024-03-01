1c4beda28cf801e8bd489e5b529071e6393e0173
Dan Kinsley <dan@thechorus.xyz>
2024-03-01 11:32:15 -0500

    webhook debug

--- apps/cake/src/app/api/webhooks/stripe/route.ts
+++ apps/cake/src/app/api/webhooks/stripe/route.ts
@@ -2,8 +2,10 @@ import stripe, { Stripe } from "stripe";
 import { handleEvent } from "./handleEvent";
 
 const webhookSecret = process.env["STRIPE_WEBHOOK_SIGNING_SECRET"]!;
-
 export async function POST(req: Request) {
+  console.log("incoming stripe webhook", {
+    signingSecret: webhookSecret.substring(0, 10),
+  });
   const payload = await req.text();
   const sig = req.headers.get("stripe-signature");
   if (!sig) {
