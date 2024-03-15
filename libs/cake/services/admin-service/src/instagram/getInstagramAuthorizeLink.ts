f2b30a17d96cc684a1879399e970377c46a10228
Dan Kinsley <dan@thechorus.xyz>
2024-03-14 21:14:25 -0400

    fix instagram auth url

--- libs/cake/services/admin-service/src/instagram/getInstagramAuthorizeLink.ts
+++ libs/cake/services/admin-service/src/instagram/getInstagramAuthorizeLink.ts
@@ -4,5 +4,5 @@ export function getInstagramAuthorizeLink(slug: string) {
   const redirectURI = getInstagramRedirectUrl();
   const clientId = process.env["INSTAGRAM_APP_ID"];
   const scope = "user_profile,user_media";
-  return `https://api.instagram.com/oauth/authorize?client_key=${clientId}&redirect_uri=${redirectURI}&scope=${scope}&response_type=code&state=${slug}`;
+  return `https://api.instagram.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectURI}&scope=${scope}&response_type=code&state=${slug}`;
 }
