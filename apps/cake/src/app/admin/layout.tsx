d083069df27dcc3d763bfc92ccadafd6bb8ff692
Dan Kinsley <dan@thechorus.xyz>
2024-03-21 15:34:59 -0400

    adjust superadmin checker

--- apps/cake/src/app/admin/layout.tsx
+++ apps/cake/src/app/admin/layout.tsx
@@ -1,5 +1,25 @@
+import { auth } from "@clerk/nextjs";
 import { AdminSectionLayout } from "@danklabs/cake/admin";
+import { superadmin } from "@danklabs/cake/services/admin-service";
+import { redirect } from "next/navigation";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  if (!(await isSuperAdmin())) {
+    return redirect("/");
+  }
   return <AdminSectionLayout>{children}</AdminSectionLayout>;
+}
+
+async function isSuperAdmin() {
+  const { userId } = auth();
+  if (!userId) {
+    return false;
+  }
+
+  const superAdmins = await superadmin.cachedGetSuperAdmins();
+  return superAdmins.map((a) => a.iam).includes(userId);
 }
--- apps/cake/src/middleware.ts
+++ apps/cake/src/middleware.ts
@@ -17,33 +17,6 @@ export default authMiddleware({
     "/terms-and-conditions",
     "/privacy-policy",
   ],
-  async afterAuth(auth, req, evt) {
-    // Allow users visiting public routes to access them
-    if (auth.isPublicRoute) {
-      return NextResponse.next();
-    }
-
-    // Handle users who aren't authenticated
-    if (!auth.userId) {
-      return redirectToSignIn({ returnBackUrl: req.url });
-    }
-
-    const user = await clerkClient.users.getUser(auth.userId);
-    if (!user) {
-      return redirectToSignIn({ returnBackUrl: req.url });
-    }
-    // restrict admin routes
-    if (
-      req.nextUrl.pathname.startsWith("/admin") &&
-      user.privateMetadata["role"] !== "admin"
-    ) {
-      const homeURL = new URL("/", req.url);
-      return NextResponse.redirect(homeURL);
-    }
-
-    // Allow users visiting public routes to access them
-    return NextResponse.next();
-  },
 });
 
 export const config = {
