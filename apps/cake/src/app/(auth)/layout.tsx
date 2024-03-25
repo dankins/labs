8e48d56a08d4cd366974922b9076f8a5f9e45e3d
Dan Kinsley <dan@thechorus.xyz>
2024-03-25 16:45:37 -0400

    remove Patissier

--- apps/cake/src/app/(auth)/layout.tsx
+++ apps/cake/src/app/(auth)/layout.tsx
@@ -1,11 +1,9 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { AppShell } from "@danklabs/cake/pattern-library/core";
-import { Patissier } from "./Patissier";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
-      <Patissier />
       <AppShell authComponent={<></>}>{children}</AppShell>
     </ClerkProvider>
   );
