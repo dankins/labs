2f573d464e6548261dd3278e0f9c6ac2861537f0
Dan Kinsley <dan@thechorus.xyz>
2024-03-27 19:02:04 -0400

    rename image page to have png

--- libs/cake/foyer/src/generateMetadata.ts
+++ libs/cake/foyer/src/generateMetadata.ts
@@ -4,20 +4,22 @@ import { Metadata } from "next";
 export async function generateMetadata(inviteCode?: string): Promise<Metadata> {
   if (!inviteCode) {
     return {
+      metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL!),
       title: `Cake Invite `,
       description: "Join Me on Cake!",
       openGraph: {
-        images: [`/invitation/opengraph-image`],
+        images: [`/invitation/opengraph-imag.png`],
       },
     };
   }
   const invite = await invitations.getByCode.cached(inviteCode);
   if (!invite) {
     return {
+      metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL!),
       title: `Cake Invite`,
       description: "Join Me on Cake!",
       openGraph: {
-        images: [`/invitation/opengraph-image`],
+        images: [`/invitation/opengraph-image.png`],
       },
     };
   }
@@ -27,7 +29,7 @@ export async function generateMetadata(inviteCode?: string): Promise<Metadata> {
     title: `Cake Invitation`,
     description: `${invite.recipientName}, join me on Cake!`,
     openGraph: {
-      images: [`/invitation/opengraph-image?code=${inviteCode}`],
+      images: [`/invitation/opengraph-image.png?code=${inviteCode}`],
     },
   };
 }
