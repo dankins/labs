7f94925b8c443863c105ccc34b45604008749654
Dan Kinsley <dan@thechorus.xyz>
2024-04-18 09:54:51 -0400

    put profile on account homepage

--- libs/cake/members/account/src/AccountPage.tsx
+++ libs/cake/members/account/src/AccountPage.tsx
@@ -2,6 +2,8 @@ import { UserIcon } from "@danklabs/pattern-library/core";
 import Link from "next/link";
 import { Menu } from "./Menu";
 
+import { ProfilePage } from "./profile/ProfilePage";
+
 export function AccountPage() {
-  return <div className="p-4"></div>;
+  return <ProfilePage />;
 }
