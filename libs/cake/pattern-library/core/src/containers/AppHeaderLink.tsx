0163413c227cd2773064bbed43c78467f5c0e0a6
Dan Kinsley <dan@thechorus.xyz>
2024-04-17 23:33:29 -0400

    better way to close the menu

--- libs/cake/pattern-library/core/src/containers/AppHeaderLink.tsx
+++ libs/cake/pattern-library/core/src/containers/AppHeaderLink.tsx
@@ -3,15 +3,26 @@ import classNames from "classnames";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import styles from "./AppHeader.module.scss";
+import { useEffect, useState } from "react";
 
 export function AppHeaderLink({
   children,
   ...props
 }: React.ComponentProps<typeof Link>) {
   const pathname = usePathname();
+  const [changing, setChanging] = useState(false);
+
+  useEffect(() => {
+    if (props.href === pathname && changing) {
+      setChanging(false);
+      document.getElementById("side-menu")?.click();
+    }
+  }, [pathname, changing]);
+
   function handleClick() {
-    document.getElementById("side-menu")?.click();
+    setChanging(true);
   }
+
   return (
     <Link
       className={classNames(
