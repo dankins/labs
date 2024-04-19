ea7c7bbbc721c4566552a5ee05006c3df9fba90d
Dan Kinsley <dan@thechorus.xyz>
2024-04-19 16:03:28 -0400

    allow redemption even if there is no memberid

--- libs/cake/services/admin-service/src/invitations/incrementRedemptions.ts
+++ libs/cake/services/admin-service/src/invitations/incrementRedemptions.ts
@@ -8,21 +8,19 @@ export async function incrementRedemptions(invitationId: string) {
   if (!invitation) {
     throw new Error("invitation not found");
   }
-  if (!invitation.memberId) {
-    throw new Error("invitation does not have a memberId");
-  }
   await db
     .update(invitations)
     .set({ redemptions: (invitation.redemptions || 0) + 1 })
     .where(eq(invitations.id, invitationId));
 
-  const member = await db.query.members.findFirst({
-    where: eq(membersModel.id, invitation.memberId),
-  });
-  if (!member) {
-    throw new Error("member not found");
-  }
-
   getInvitation.clearCache(invitationId);
-  members.member.invitations.clearInvitationsCache(member.iam);
+  if (invitation.memberId) {
+    const member = await db.query.members.findFirst({
+      where: eq(membersModel.id, invitation.memberId),
+    });
+    if (!member) {
+      throw new Error("member not found");
+    }
+    members.member.invitations.clearInvitationsCache(member.iam);
+  }
 }
