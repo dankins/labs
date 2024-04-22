3181a2e1106050850b55c89593638bd9d4b6e9d4
Dan Kinsley <dan@thechorus.xyz>
2024-04-22 14:33:56 -0400

    create invitations log

--- libs/cake/services/admin-service/src/invitations/create.ts
+++ libs/cake/services/admin-service/src/invitations/create.ts
@@ -6,15 +6,14 @@ const NEW_MEMBER_MAX_REDEMPTIONS = 1;
 
 export async function create(memberId: string, amount?: number) {
   type NewInvitation = typeof invitations.$inferInsert;
+  console.log("Creating invitations for member", memberId, amount);
   const newInvitatations: NewInvitation[] = new Array(
     amount || NEW_MEMBER_INVITATIONS
-  )
-    .fill(undefined)
-    .map((_) => ({
-      memberId,
-      redemptions: 0,
-      maxRedemptions: NEW_MEMBER_MAX_REDEMPTIONS,
-    }));
+  ).fill({
+    memberId,
+    redemptions: 0,
+    maxRedemptions: NEW_MEMBER_MAX_REDEMPTIONS,
+  });
   await db.insert(invitations).values(newInvitatations);
 
   members.member.invitations.clearInvitationsCache(memberId);
