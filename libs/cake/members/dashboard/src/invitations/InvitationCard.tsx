2acc32a51a8c2e85d125ac9fa132518191a0ab08
Dan Kinsley <dan@thechorus.xyz>
2024-04-23 14:26:33 -0400

    expired invitation card

--- libs/cake/members/dashboard/src/invitations/InvitationCard.tsx
+++ libs/cake/members/dashboard/src/invitations/InvitationCard.tsx
@@ -67,28 +67,30 @@ function PendingInvitation({ invitation }: { invitation: MemberInvitation }) {
 
 function ExpiredInvitation({ invitation }: { invitation: MemberInvitation }) {
   return (
-    <Container>
-      <div className="flex flex-row items-center gap-1">
-        <ClockIcon className="fill-primary text-xl" />
-        <span className="text-xs">expired</span>
-      </div>
-      <div>
-        <SectionHeading>Pending invitations</SectionHeading>
-        <div className="text-xs font-medium mt-1">
-          Track invitations you have sent to join Cake.
-        </div>
-      </div>
-      <div>
-        <Button
-          background="[#B83CA4]"
-          textColor="white"
-          rounded="full"
-          className="text-xs"
+    <div className="p-4 border border-[#E1DED9] bg-[#FBFAF8] flex flex-col items-center justify-center  ">
+      <LogoMark className="text-xl fill-dark" />
+      <Heading3 className="uppercase">Invite Shared</Heading3>
+      <Paragraph3 className="text-lg font-selva">
+        {invitation.recipientName}
+      </Paragraph3>
+      <Caption1 className="uppercase text-secondary">
+        Expired {dayjs().to(dayjs(invitation.expiration))}
+      </Caption1>
+      <div className="my-4 flex flex-row gap-2 items-center justify-center">
+        <PrimaryButton
+          className="uppercase"
+          href={`?action=share-invite&inviteId=${invitation.id}&screen=refresh-expired`}
         >
-          Manage
-        </Button>
+          RESEND
+        </PrimaryButton>
+        <SecondaryButton
+          className="uppercase"
+          href={`?action=cancel-invite&inviteId=${invitation.id}`}
+        >
+          CANCEL
+        </SecondaryButton>
       </div>
-    </Container>
+    </div>
   );
 }
 function AcceptedInvitation({ invitation }: { invitation: MemberInvitation }) {
