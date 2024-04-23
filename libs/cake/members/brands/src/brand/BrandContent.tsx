46776d413a60c7cb2bcfd2c84a89095878d2c755
Dan Kinsley <dan@thechorus.xyz>
2024-04-22 20:47:40 -0400

    logo sizing

--- libs/cake/members/brands/src/brand/BrandContent.tsx
+++ libs/cake/members/brands/src/brand/BrandContent.tsx
@@ -110,10 +110,10 @@ async function Component({ slug }: { slug: string }) {
                 width={250}
                 height={250}
                 style={{
-                  height: "auto",
-                  width: "100%",
                   maxHeight: "80px",
                   maxWidth: "175px",
+                  objectFit: "contain",
+                  objectPosition: "left bottom",
                 }}
                 className="invert md:invert-0"
               />
