ad0b865cc6e410bedeba742d0c53b336b88d850d
Dan Kinsley <dan@thechorus.xyz>
2024-04-24 16:14:04 -0400

    logo

--- apps/cake/src/app/page.tsx
+++ apps/cake/src/app/page.tsx
@@ -1,9 +1,13 @@
 import classNames from "classnames";
 import { auth } from "@clerk/nextjs/server";
 import { PageView } from "@danklabs/cake/events";
-import { Heading3, Logo } from "@danklabs/cake/pattern-library/core";
+import { Heading3, LogoIcon } from "@danklabs/cake/pattern-library/core";
 import Link from "next/link";
-import { PrimaryButton, RightArrow } from "@danklabs/pattern-library/core";
+import {
+  PrimaryButton,
+  RightArrow,
+  SecondaryButton,
+} from "@danklabs/pattern-library/core";
 
 export default function Page() {
   const { userId: iam } = auth();
@@ -16,14 +20,18 @@ export default function Page() {
           className={classNames("flex flex-col items-center gap-6 container")}
         >
           <div className="max-w-[570px] w-full">
-            <Logo className="w-full" />
+            <LogoIcon className="w-full" />
           </div>
           <Heading3>Shopping is about to get sweeter.</Heading3>
           <div className="mt-[40px]">
             {!!iam ? (
-              <Link href={`/collection`}>
-                Continue to Collection <RightArrow />
-              </Link>
+              <PrimaryButton
+                href={`/collection`}
+                icon={<RightArrow />}
+                iconPosition="right"
+              >
+                Continue to Collection
+              </PrimaryButton>
             ) : (
               <PrimaryButton href={`/sign-in`}>Member Sign In</PrimaryButton>
             )}
