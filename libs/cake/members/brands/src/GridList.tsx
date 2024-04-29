2dc8a4fcd1a18a25145101fa844c351d06ff7ba3
Dan Kinsley <dan@thechorus.xyz>
2024-04-29 09:49:11 -0400

    gap on mobile

--- libs/cake/members/brands/src/GridList.tsx
+++ libs/cake/members/brands/src/GridList.tsx
@@ -104,7 +104,7 @@ function BrandGrid({
           <SecondaryButton icon={<SortIcon />} />
         </div>
       </div>
-      <div className="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 md:gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-2 md:gap-4">
         {brands.map((b) => (
           <GridItem
             key={b.db.id}
