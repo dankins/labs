5ced361880598f7805450445b1da6bca4a158272
Dan Kinsley <dan@thechorus.xyz>
2024-04-29 09:41:46 -0400

    available invite card

--- libs/cake/members/dashboard/src/invitations/AvailableInviteCard.tsx
+++ libs/cake/members/dashboard/src/invitations/AvailableInviteCard.tsx
@@ -15,21 +15,20 @@ export function AvailableInviteCard({
   totalCount: number;
   availableId: string;
 }) {
-  const availableMessage = availableCount > 1 ? "Invites" : "Invite";
   return (
     <div className="p-4 border border-[#E1DED9] bg-[#FBFAF8] flex flex-col items-center justify-center  ">
-      <LogoMark className="text-xl fill-dark" />
-      <Heading3 className="font-supreme font-normal text-dark uppercase text-[27px]">
+      <LogoMark className="m-2 h-[18px] w-[18px] fill-dark" />
+      <Heading3 className="font-supreme font-normal text-dark uppercase text-[22px]">
         Invite A Friend
       </Heading3>
       <Paragraph3 className="text-secondary text-lg font-selva font-normal text-[18px]">
         (a really good one)
       </Paragraph3>
       <Caption3 className="uppercase font-normal text-primary py-[8px]">
-        {availableCount} {availableMessage}
+        {availableCount} Remaining
       </Caption3>
       <PrimaryButton
-        className="my-4 uppercase"
+        className="my-2 uppercase"
         href={`?action=share-invite&inviteId=${availableId}`}
       >
         Share Wisely
