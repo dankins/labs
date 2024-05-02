0401c075fe994b27cf663201cabaf976e23992b0
Dan Kinsley <dan@thechorus.xyz>
2024-05-02 09:30:15 -0400

    create profile when adding brand

--- libs/cake/db/drizzle/0029_careful_forge.sql
+++ libs/cake/db/drizzle/0029_careful_forge.sql
@@ -1,7 +1,2 @@
 ALTER TABLE "connections" ADD CONSTRAINT "connections_member_id_follower_id_unique" UNIQUE("member_id","follower_id");
 
-insert into profiles (id, parent_type, parent_id, username, created_at, updated_at) 
-select gen_random_uuid() as id, 'brand' as parent_type, id as parent_id, slug as username, NOW() as created_at, now() as updated_at from brands;
-
-insert into profiles (id, parent_type, parent_id, username, created_at, updated_at) 
-select gen_random_uuid() as id, 'member' as parent_type, id as parent_id, 'member-' || lower(substr(md5(random()::text), 1, 6)) as username, NOW() as created_at, now() as updated_at from members;
